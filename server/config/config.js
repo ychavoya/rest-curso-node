62f7a42a248c47bbaa0aacbad6ae2ec96f0c5b56
Yael Chavoya <realidadvirtualwm@gmail.com>
2018-08-08 14:23:39 -0500

    Variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:iamroot123@ds215822.mlab.com:15822/woomber-cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
